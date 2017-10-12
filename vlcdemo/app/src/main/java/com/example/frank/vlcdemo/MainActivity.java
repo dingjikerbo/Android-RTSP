32255e0b8b1cdba7e4c21933a1be21371304e7fc
frank <frank@frank.(none)>
2017-10-12 08:59:24 +0800

    fd

--- vlcdemo/app/src/main/java/com/example/frank/vlcdemo/MainActivity.java
+++ vlcdemo/app/src/main/java/com/example/frank/vlcdemo/MainActivity.java
@@ -1,27 +1,36 @@
 package com.example.frank.vlcdemo;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.ImageView;
 
 import org.videolan.libvlc.IVLCVout;
 import org.videolan.libvlc.LibVLC;
 import org.videolan.libvlc.Media;
 import org.videolan.libvlc.MediaPlayer;
+import org.videolan.libvlc.MediaPlayCallback;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.ArrayList;
 
 public class MainActivity extends Activity implements SurfaceHolder.Callback {
 
-    private static final String URL = "rtsp://192.168.10.61:8554/test.mkv";
+//    private static final String URL = "rtsp://192.168.1.100:8554/test.mkv";
+    public static final String URL = "rtsp://admin:admin123@10.31.11.79:554/cam/realmonitor?channel=1@subtype=0";
 
     private SurfaceView mSurfaceView;
 
     private LibVLC mVlc;
 
     private MediaPlayer mMediaPlayer;
+
+    private ImageView mImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +39,28 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 
         mSurfaceView = findViewById(R.id.surface);
         mSurfaceView.getHolder().addCallback(this);
+
+        mImage = findViewById(R.id.image);
     }
+
+    private Bitmap bufferToBitmap(ByteBuffer buffer) {
+        final Bitmap bmp = Bitmap.createBitmap(960, 540, Bitmap.Config.ARGB_8888);
+        buffer.rewind();
+        bmp.copyPixelsFromBuffer(buffer);
+
+//        Matrix matrix = new Matrix();
+//        matrix.postScale(360f / bmp.getWidth(), 640f / bmp.getHeight());
+//        matrix.postRotate(180);
+//        Bitmap dstbmp = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), matrix, false);
+//
+//        BitmapUtils.recycle(bmp);
+
+        return bmp;
+    }
+
+    private Bitmap mBitmap;
+
+    private ByteBuffer mBuffer = ByteBuffer.allocateDirect(960 * 540 * 4).order(ByteOrder.nativeOrder());
 
     private void createPlayer() {
         releasePlayer();
@@ -42,17 +72,45 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
             options.add("-vvv"); // verbosity
             mVlc = new LibVLC(this, options);
 
+
             // Create media player
             mMediaPlayer = new MediaPlayer(mVlc);
+            mMediaPlayer.setVideoFormat("RGBA", 960, 540, 960 * 4);
+//            mMediaPlayer.setScale(0.75f);
+
+            mMediaPlayer.setVideoCallback(mBuffer, new MediaPlayCallback() {
+                @Override
+                public void onDisplay(ByteBuffer buffer) {
+                    Log.v("bush", "called here");
+
+                    buffer.rewind();
+
+                    if (mBitmap != null) {
+                        mBitmap.recycle();
+                    }
+                    mBitmap = bufferToBitmap(buffer);
+                    mImage.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Log.v("bush", String.format("bitmap size = %d,%d", mBitmap.getWidth(), mBitmap.getHeight()));
+                            mImage.setImageBitmap(mBitmap);
+                        }
+                    });
+
+                }
+            });
+
 
             // Set up video output
-            final IVLCVout vout = mMediaPlayer.getVLCVout();
-            vout.setVideoView(mSurfaceView);
-            vout.attachViews();
+//            final IVLCVout vout = mMediaPlayer.getVLCVout();
+//            vout.setVideoView(mSurfaceView);
+//            vout.attachViews();
 
-            vout.setWindowSize(1920, 1080);
+//            vout.setWindowSize(1920, 1080);
 
             Media m = new Media(mVlc, Uri.parse(URL));
+
+
             int cache = 1000;
             m.addOption(":network-caching=" + cache);
             m.addOption(":file-caching=" + cache);
